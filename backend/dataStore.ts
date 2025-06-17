0801bcf3b1213f984a772c587ea067c3a5625ac5
rdhmdhl <reid@wearthefewture.com>
2025-06-16 19:13:03 -0700

    remove unused file

--- backend/dataStore.ts
+++
@@ -1,9 +0,0 @@
-const results: Record<string, any> = {};
-
-export function saveResult(runId: string, data: any) {
-  results[runId] = data;
-}
-
-export function getResult(runId: string) {
-  return results[runId] || null;
-}
